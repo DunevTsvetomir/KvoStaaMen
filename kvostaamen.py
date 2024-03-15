8c8b252f5b13489d1aa56ce2e629aae4e81913c8
Atanas <atakan08.k@gmail.com>
2024-03-15 22:59:42 +0200

    removed usseless files

--- kvostaamen.py
+++
@@ -1,8 +0,0 @@
-from flask import Flask, render_template, request 
-
-app = Flask(__name__)
-
-@app.route("/kvostaamen", methods=["GET", "POST"])
-def home():
-    print(request.form.get("account"))
-    return render_template("kvostaamen.html")
