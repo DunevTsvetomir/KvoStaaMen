51915aa1fb7c1840ca3b35df41c5e880e83e8b8d
Atanas <atakan08.k@gmail.com>
2024-03-15 23:22:15 +0200

    removed usseless files

--- js/earth.js
+++ js/earth.js
@@ -2,11 +2,11 @@ function openCity(evt, cityName) {
     var i;
     var cities = document.getElementsByClassName("city");
     for (i = 0; i < cities.length; i++) {
+      if (cities[i].style.display === "block" && cities[i].id === cityName + "Text") {
+        cities[i].style.display = "none";
+        return;
+      }
       cities[i].style.display = "none";
-    }
-    var tablinks = document.getElementsByClassName("tablinks");
-    for (i = 0; i < tablinks.length; i++) {
-      tablinks[i].classList.remove("active");
     }
     document.getElementById(cityName + "Text").style.display = "block";
     evt.currentTarget.classList.add("active");
@@ -25,3 +25,4 @@ function openCity(evt, cityName) {
     ocean.style.display = "none";
     sea.style.display = "block";
   }
+  
